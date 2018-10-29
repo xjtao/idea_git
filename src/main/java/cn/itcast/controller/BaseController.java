eaf120355adbafdbc02191422fd7e41a11effee4
xjt <xjt@126.com>
2018-10-29 16:12:14 +0800

    Merge branch 'master' of https://github.com/xjtao/idea_git
    # Conflicts:
    #	src/main/java/cn/itcast/controller/BaseController.java

--- src/main/java/cn/itcast/controller/BaseController.java
+++ src/main/java/cn/itcast/controller/BaseController.java
@@ -3,6 +3,7 @@ package cn.itcast.controller;
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("sfjalf");
+        System.out.println("fjalfjf");
     }
 
 }
